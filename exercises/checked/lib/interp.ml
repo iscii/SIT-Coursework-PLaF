85a9b74d6d7443ca7210c16537cf46958c3fcbcc
Issac <issac.zheng9869@gmail.com>
2023-06-29 19:23:51 -0400

    last day notes

--- exercises/checked/lib/interp.ml
+++ exercises/checked/lib/interp.ml
@@ -1,22 +1,14 @@
 open Ds
+open ReM
 open Parser_plaf.Ast
 open Parser_plaf.Parser
        
 let g_store = Store.empty_store 20 (NumVal 0)
 
-let rec value_of_operand e =
-  match e with
-  | Var id -> apply_env id
-  | _ -> eval_expr e >>= fun ev ->
-    return @@ RefVal (Store.new_ref g_store ev)
-and
-  eval_expr : expr -> exp_val ea_result = fun e ->
+let rec eval_expr : expr -> exp_val ea_result = fun e ->
   match e with
   | Int(n) -> return @@ NumVal n
-  | Var(id) -> 
-    apply_env id >>=
-    int_of_refVal >>=
-    Store.deref g_store      
+  | Var(id) -> apply_env id
   | Add(e1,e2) ->
     eval_expr e1 >>=
     int_of_numVal >>= fun n1 ->
@@ -44,10 +36,9 @@ and
     then error "Division by zero"
     else return @@ NumVal (n1/n2)
   | Let(v,def,body) ->
-    eval_expr def >>= fun ev ->
-    let l = Store.new_ref g_store ev
-    in extend_env v (RefVal l) >>+
-    eval_expr body
+    eval_expr def >>= 
+    extend_env v >>+
+    eval_expr body 
   | ITE(e1,e2,e3) ->
     eval_expr e1 >>=
     bool_of_boolVal >>= fun b ->
@@ -70,38 +61,39 @@ and
     eval_expr e >>=
     pair_of_pairVal >>= fun p ->
     return @@ snd p
-  | Proc(id,_,e)  ->
+  | Proc(id,_,e)   ->
     lookup_env >>= fun en ->
     return (ProcVal(id,e,en))
   | App(e1,e2)  -> 
     eval_expr e1 >>= 
     clos_of_procVal >>= fun (id,e,en) ->
-    value_of_operand e2 >>= fun refval ->
+    eval_expr e2 >>= fun ev ->
     return en >>+
-    extend_env id refval >>+
+    extend_env id ev >>+
     eval_expr e
-    (* 
-    
-    *)
-  | Letrec([id,par,_,_,e],target) ->
-    let l = Store.new_ref g_store UnitVal in
-    extend_env id (RefVal l) >>+
-    (lookup_env >>= fun env ->
-    Store.set_ref g_store l (ProcVal(par,e,env)) >>=  fun _ ->
-    eval_expr target)
-  | Set(id,e) ->
-    eval_expr e >>= fun ev ->
-    apply_env id >>=
-    int_of_refVal >>= fun l ->
-    Store.set_ref g_store l ev >>= fun _ ->
-    return UnitVal
+  | Letrec([(id,par,_targ,_ty,e)],target) ->
+    extend_env_rec id par e >>+
+    eval_expr target 
   | BeginEnd(es) ->
     List.fold_left (fun c e -> c >>= fun _ -> eval_expr e) (return UnitVal) es
+  | NewRef(e) ->
+    eval_expr e >>= fun ev ->
+    return @@ RefVal (Store.new_ref g_store ev)
+  | DeRef(e) ->
+    eval_expr e >>=
+    int_of_refVal >>= 
+    Store.deref g_store 
+  | SetRef(e1,e2) ->
+    eval_expr e1 >>=
+    int_of_refVal >>= fun v1 ->
+    eval_expr e2 >>= fun v2 ->
+    Store.set_ref g_store v1 v2 >>= fun _ ->
+    return UnitVal
   | Debug(_e) ->
     string_of_env >>= fun str_env ->
     let str_store = Store.string_of_store string_of_expval g_store 
     in (print_endline (str_env^"\n"^str_store);
-    error "Reached breakpoint")
+        error "Reached breakpoint")
   | _ -> failwith ("Not implemented: "^string_of_expr e)
 
 let eval_prog (AProg(_,e)) =
@@ -111,5 +103,4 @@ let eval_prog (AProg(_,e)) =
 let interp (s:string) : exp_val result =
   let c = s |> parse |> eval_prog
   in run c
-
 
